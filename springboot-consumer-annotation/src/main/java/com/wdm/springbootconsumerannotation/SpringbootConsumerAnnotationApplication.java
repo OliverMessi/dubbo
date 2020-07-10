0fcb7a85ab3b1490760052581dd59622965f28ef
OliverMessi <562376662@qq.com>
2020-07-10 22:46:56 +0800

    创建springboot-consumer-annotation模块

---
+++ springboot-consumer-annotation/src/main/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.wdm.springbootconsumerannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConsumerAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConsumerAnnotationApplication.class, args);
+	}
+
+}
---
+++ springboot-consumer-annotation/src/test/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdm.springbootconsumerannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConsumerAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
