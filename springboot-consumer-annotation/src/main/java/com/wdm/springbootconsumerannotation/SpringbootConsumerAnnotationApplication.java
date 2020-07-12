b1469fb6674dbf19b164f93faaaffc9b5f02b476
OliverMessi <562376662@qq.com>
2020-07-12 23:35:19 +0800

    修改springboot-provider-xml模块

--- springboot-consumer-annotation/src/main/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplication.java
+++ springboot-consumer-annotation/src/main/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplication.java
@@ -1,13 +1,15 @@
 package com.wdm.springbootconsumerannotation;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class SpringbootConsumerAnnotationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootConsumerAnnotationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConsumerAnnotationApplication.class, args);
+    }
 
 }
--- springboot-consumer-annotation/src/test/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplicationTests.java
+++ springboot-consumer-annotation/src/test/java/com/wdm/springbootconsumerannotation/SpringbootConsumerAnnotationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.wdm.springbootconsumerannotation;
 
-import org.junit.jupiter.api.Test;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.wdm.service.IUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootConsumerAnnotationApplicationTests {
+public class SpringbootConsumerAnnotationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Reference
+    private IUserService userService;
+    @Test
+    public void contextLoads() {
+        System.out.println(userService.hello());
+    }
 
 }
